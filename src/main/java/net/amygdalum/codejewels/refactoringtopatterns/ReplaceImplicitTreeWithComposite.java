5d8de91ab44573e2e8ef8a1261d79cb6a9456bc7
Stefan Mandel <Stefan.Mandel@andrena.de>
2021-11-14 11:11:28 +0100

    inline variables

--- src/main/java/net/amygdalum/codejewels/refactoringtopatterns/ReplaceImplicitTreeWithComposite.java
+++ src/main/java/net/amygdalum/codejewels/refactoringtopatterns/ReplaceImplicitTreeWithComposite.java
@@ -5,19 +5,18 @@ public class ReplaceImplicitTreeWithComposite {
 	public String serializeOrders() {
 		return new ConstantNode().toString();
 	}
-	
+
 	public static class ConstantNode {
-		
+
 		@Override
 		public String toString() {
-			String orders1 = "<orders>";
-			String orders2 = orders1 + "<order number='123'>";
-			String orders3 = orders2 + "<item number='x1786'>";
-			String orders4 = orders3 + "carDoor";
-			String orders5 = orders4 + "</item>";
-			String orders6 = orders5 + "</order>";
-			String orders7 = orders6 + "</orders>";
-			return orders7;
+			return "<orders>"
+				+ "<order number='123'>"
+				+ "<item number='x1786'>"
+				+ "carDoor"
+				+ "</item>"
+				+ "</order>"
+				+ "</orders>";
 		}
 	}
 }
