980b697e94e0034eabbc0e4f40c1f84681997a0f
Stefan Mandel <Stefan.Mandel@andrena.de>
2021-11-14 11:07:42 +0100

    Extract code into class

--- src/main/java/net/amygdalum/codejewels/refactoringtopatterns/ReplaceImplicitTreeWithComposite.java
+++ src/main/java/net/amygdalum/codejewels/refactoringtopatterns/ReplaceImplicitTreeWithComposite.java
@@ -3,13 +3,21 @@ package net.amygdalum.codejewels.refactoringtopatterns;
 public class ReplaceImplicitTreeWithComposite {
 
 	public String serializeOrders() {
-		String orders = "<orders>";
-		orders += "<order number='123'>";
-		orders += "<item number='x1786'>";
-		orders += "carDoor";
-		orders += "</item>";
-		orders += "</order>";
-		orders += "</orders>";
-		return orders;
+		return new ConstantNode().toString();
+	}
+	
+	public static class ConstantNode {
+		
+		@Override
+		public String toString() {
+			String orders = "<orders>";
+			orders += "<order number='123'>";
+			orders += "<item number='x1786'>";
+			orders += "carDoor";
+			orders += "</item>";
+			orders += "</order>";
+			orders += "</orders>";
+			return orders;
+		}
 	}
 }
